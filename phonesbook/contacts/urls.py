23a04092a0ee0d7ac8d9e1f8df1dc9fed32255f9
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-10-01 14:37:51 +0300

    Add contact removing

--- phonesbook/contacts/urls.py
+++ phonesbook/contacts/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.GetContactsView.as_view(), name='contacts'),
     path('add/', views.CreateContactView.as_view(), name='add_contact'),
+    path('<int:pk>/delete/', views.DeleteContactView.as_view(), name='delete_contact')
 ]
 
