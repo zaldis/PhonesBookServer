b0801f984d77f6a34ae560641cde48da96d2acdb
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2022-10-08 17:48:57 +0300

    Add permissions
    - Ability to add contacts from the file
    - Ability to download contacts in JSON file
    - Add permissions for adding/removing contacts

--- phonesbook/api/urls.py
+++ phonesbook/api/urls.py
@@ -4,6 +4,9 @@ from rest_framework.authtoken import views as drf_views
 from . import views
 
 
+app_name = 'api'
+
+
 urlpatterns = [
     path('contacts/', views.GetContactsAPIView.as_view(), name='contacts'),
     path('token/', drf_views.obtain_auth_token),
--- phonesbook/contacts/urls.py
+++ phonesbook/contacts/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from . import views
 
 
+app_name = 'contacts'
+
+
 urlpatterns = [
-    path('', views.GetContactsView.as_view(), name='contacts'),
-    path('add/', views.CreateContactView.as_view(), name='add_contact'),
-    path('<int:pk>/delete/', views.DeleteContactView.as_view(), name='delete_contact')
+    path('', views.GetContactsView.as_view(), name='all'),
+    path('add/', views.CreateContactView.as_view(), name='add'),
+    path('add-from-file/', views.CreateContactsView.as_view(), name='add_from_file'),
+    path('download/', views.DownloadContactsView.as_view(), name='download'),
+    path('<int:pk>/delete/', views.DeleteContactView.as_view(), name='delete')
 ]
 
