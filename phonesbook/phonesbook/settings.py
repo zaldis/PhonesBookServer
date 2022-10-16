aded94d81c8d6fec7f5f92042cd313b72bf74445
Denys Zaluzhnyi <38065313+zaldis@users.noreply.github.com>
2022-10-16 11:09:56 +0300

    Add Makefile
    - Add Makefile
    - Add favicon

--- phonesbook/phonesbook/settings.py
+++ phonesbook/phonesbook/settings.py
@@ -150,6 +150,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'build-static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
